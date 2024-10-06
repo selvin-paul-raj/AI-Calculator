66880a7478c83143cb1affd477a72871bc4c9af8
--replace-all <sprhackz003@gmail.com>
2024-10-06 20:19:30 +0530

    add:simple draw

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter,RouterProvider} from "react-router-dom";
+import "@mantine/core/styles.css"
+import { MantineProvider } from "@mantine/core";
+import Home from "@/pages/Home";
+const paths=[
+  {
+    path:'/' ,
+    element:(
+      <Home/>
+    ),
+  },
+]
+
+const BrowserRouter = createBrowserRouter(paths);
+const App = ()=>{
+  return(
+    <MantineProvider>
+      <RouterProvider router={BrowserRouter}/>
+    </MantineProvider>
+  )
+}
+export default App;
